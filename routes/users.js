95c0461ed79124c4bc7f519e1e19da832eaa59e7
bhargav_dobariya <evital.bhargav@gmail.com>
2024-06-27 19:12:53 +0530

    addded post api

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const router = express.Router();
+import { v4 as uuidv4 } from 'uuid';
 
 //mock database
 
@@ -20,6 +21,18 @@ const users = [
 router.get('/', (req, res) => {
     console.log('[GET ROUTE]');
     res.send(users);
+})
+
+router.post('/', (req, res) => {
+    const user = req.body;
+    users.push({ ...user, id: uuidv4() });
+    res.send(`User with the name ${user.firstName} added to the database!`);
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const foundUser = users.find((user) => user.id === id);
+    res.send(foundUser);
 })
 
 export default router;
